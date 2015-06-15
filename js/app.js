35c912722b89265bd063c9220340e623b3886a0f
Ed Gilmore <edgilmore@origamiowl.com>
2015-06-15 12:39:31 -0700

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by edgilmore on 6/15/2015.
+ */
+var app = angular.module('PizzaPlanetApp',[]);
---
+++ js/controllers/MainController.js
@@ -0,0 +1,57 @@
+/**
+ * Created by edgilmore on 6/15/2015.
+ */
+app.controller('MainController',['$scope', function ($scope) {
+    $scope.today = new Date();
+    $scope.appetizers = [
+        {
+            name: 'Caprese',
+            description: 'Mozzarella, tomatoes, basil, balsamic, glaze',
+            price: 4.95
+        },
+        {
+            name: 'Mozzarella Sticks',
+            description: 'Served with marinara sauce.',
+            price: 3.95
+        },
+        {
+            name: 'Brushetta',
+            Description: 'Grilled bread garlic, tomatoes, olive oil.',
+            price: 4.95
+        }
+    ];
+    $scope.mains = [
+        {
+            name: 'Margherita',
+            description: 'Marinara sauce, mozzarella, fresh basil.',
+            price: 6.95
+        },
+        {
+            name: 'Pepperoni',
+            description: 'Pepperoni, marinara sauce, mozzarella, fresh oregano.',
+            price: 7.95
+        },
+        {
+            name: 'Rustica',
+            description: 'Marinara sauce, kalamata olives, roasted garlic, fresh oregano.',
+            price: 8.95
+        }
+    ];
+    $scope.extras = [
+        {
+            name: 'Breadsticks',
+            description: 'Served with marinara sauce.',
+            price: 4.95
+        },
+        {
+            name: 'Soup of the day',
+            description: 'Take a chance.',
+            price: 4.95
+        },
+        {
+            name: 'Buffalo Wings',
+            description: 'Feel the power of wings.',
+            price: 6.95
+        }
+    ];
+}]);
